8d813586a03e03e095da135e4e38e89f58cc6ba8
Ashraf Islam <ashraful62@gmail.com>
2020-03-07 17:58:51 -0500

    Adds header component and scss styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app for Ashraf';
+  componentTitle="I am app component from component.ts";
+
+  clickHandler(){
+    alert("I'm clicked");
+  }
 }
